fa6c70cbc307b2ff355483563c8aed4dc1929e9a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-12-29 17:05:53 +0100

    added is number narcissistic

---
+++ src/main/java/tasks/IsNumberNarcissistic.java
@@ -0,0 +1,43 @@
+package tasks;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+/**
+ * A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+ *
+ * For example, take 153 (3 digits), which is narcisstic:
+ *
+ *     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+ * and 1652 (4 digits), which isn't:
+ *
+ *     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+ * The Challenge:
+ *
+ * Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+ *
+ * Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.*/
+public class IsNumberNarcissistic {
+    public static void main(String[] args) {
+        System.out.println(isNarcissistic(153));
+        System.out.println(isNarcissistic(1634));
+        System.out.println(isNarcissistic(112));
+    }
+
+    public static boolean isNarcissistic(int number) {
+        if (number <= 0) return false;
+        String str = Integer.toString(number);
+
+        List<Integer> list = Stream.of(str.split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        int result = list.stream()
+                .map(n -> Math.pow(n, list.size()))
+                .mapToInt(Double::intValue)
+                .reduce(0, Integer::sum);
+        System.out.println(result);
+        return result == number;
+    }
+}
