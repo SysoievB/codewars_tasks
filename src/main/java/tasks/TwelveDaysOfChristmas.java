05878e57e1dd284012794020f84d8e92d42da5a9
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-12-30 16:12:38 +0100

    added TwelveDaysOfChristmas

--- src/main/java/tasks/TwelveDaysOfChristmas.java
+++ src/main/java/tasks/TwelveDaysOfChristmas.java
@@ -1,6 +1,8 @@
 package tasks;
 
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * DESCRIPTION:
@@ -30,13 +32,23 @@ import java.util.Comparator;
 public class TwelveDaysOfChristmas {
     public static void main(String[] args) {
 
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher("12 drummers drumming,");
+        while (m.find()) {
+            System.out.println(m.group());
+        }
     }
 
     public static class HelpSaveChristmas implements Comparator<String> {
 
         @Override
         public int compare(final String line1, final String line2) {
-            // Your comparison code goes here!
+            Pattern p = Pattern.compile("\\d+");
+            Matcher m1 = p.matcher(line1);
+            Matcher m2 = p.matcher(line2);
+            while (m1.find() && m2.find()) {
+                return Integer.parseInt(m1.group()) > Integer.parseInt(m2.group()) ? 1 : 0;
+            }
             return 0;
         }
     }
